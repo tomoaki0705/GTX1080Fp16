72bb2f3abac6f426571bfd707f685fc575a76d8d
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-08-17 07:44:33 +0900

    modify to half2

--- multiply.cu
+++ multiply.cu
@@ -8,24 +8,19 @@ extern "C" void
 launchCudaProcessFloat(dim3 grid, dim3 block, int sbytes,
 					float *gain, float *imageInput, float *imageOutput);
 
-// __half2 __hmul2(const __half2 a, const __half2 b);
-
 __global__ void
-cudaProcessHalf(float *dst, short *_gain, float *src, int imgW)
+cudaProcessHalf(half2 *dst, half2 *gain, half2 *src, int imgW)
 {
-	half *gain = (half*)_gain;
 	int tx = threadIdx.x;
 	int ty = threadIdx.y;
 	int bw = blockDim.x;
 	int bh = blockDim.y;
 	int x = blockIdx.x*bw + tx;
 	int y = blockIdx.y*bh + ty;
+	int px = (y*imgW+x);
 
-	half a = gain[y*imgW+x];
-	float gainFloat;
-	gainFloat = __half2float(a);
+	half2 g = gain[px];
+	half2 i = src[px];
 
-	float b = (float)src[y*imgW+x];
-
-	dst[y*imgW+x] = b * gainFloat;
+	dst[px] = __hmul2(g, i);
 }

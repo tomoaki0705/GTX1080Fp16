de106b03ae4e2006635a65196830591db2ea549f
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-08-17 18:16:02 +0900

    separate half and half2

--- multiply.cu
+++ multiply.cu
@@ -2,14 +2,14 @@
 
 extern "C" void
 launchCudaProcessHalf(dim3 grid, dim3 block, int sbytes,
-					short *gain, float *imageInput, float *imageOutput);
+					short *gain, short *imageInput, short *imageOutput);
 
 extern "C" void
 launchCudaProcessFloat(dim3 grid, dim3 block, int sbytes,
 					float *gain, float *imageInput, float *imageOutput);
 
 __global__ void
-cudaProcessHalf(half2 *dst, half2 *gain, half2 *src, int imgW)
+cudaProcessHalf(half *dst, half *gain, half *src, int imgW)
 {
 	int tx = threadIdx.x;
 	int ty = threadIdx.y;
@@ -17,10 +17,10 @@ cudaProcessHalf(half2 *dst, half2 *gain, half2 *src, int imgW)
 	int bh = blockDim.y;
 	int x = blockIdx.x*bw + tx;
 	int y = blockIdx.y*bh + ty;
-	int px = (y*imgW+x);
+	int px = y*imgW+x;
 
-	half2 g = gain[px];
-	half2 i = src[px];
+	half g = gain[px];
+	half i = src[px];
 
-	dst[px] = __hmul2(g, i);
+	dst[px] = __hmul(g, i);
 }

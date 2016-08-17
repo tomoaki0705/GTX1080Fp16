8a3660d2d2dfdcd765e17d266aab171e415e13f4
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-08-18 06:07:59 +0900

    add half version

--- multiply.cpp
+++ multiply.cpp
@@ -18,26 +18,51 @@ template<typename T> void launchCudaProcess(int imgW, int imgH, int gridX, int g
 template<> void
 launchCudaProcess<float>(int imgW, int imgH, int gridX, int gridY)
 {
-	float* imageFloat, *gainFloat, *dstFloat;
+	float* srcImage, *gainImage, *dstImage;
 	int s = imgW*imgH;
 
-	cudaMalloc((float**)&imageFloat, (s*sizeof(float)));
-	cudaMalloc((float**)&gainFloat, (s*sizeof(float)));
-	cudaMalloc((float**)&dstFloat, (s*sizeof(float)));
+	cudaMalloc((float**)&srcImage, (s*sizeof(float)));
+	cudaMalloc((float**)&gainImage, (s*sizeof(float)));
+	cudaMalloc((float**)&dstImage, (s*sizeof(float)));
 
 	dim3 block(gridX, gridY, 1);
 	dim3 grid(imgW / block.x, imgH / block.y, 1);
 
 	auto start = std::chrono::system_clock::now();
-	launchCudaProcessFloat0(grid, block, 0, gainFloat, imageFloat, dstFloat, imgW);
+	launchCudaProcessFloat0(grid, block, 0, gainImage, srcImage, dstImage, imgW);
 	auto end  = std::chrono::system_clock::now();
 	auto dur = end - start;
 	int msec = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
 	std::cout << msec << " [us] consumed" << std::endl;
 
-	cudaFree((void*)imageFloat);
-	cudaFree((void*)gainFloat);
-	cudaFree((void*)dstFloat);
+	cudaFree((void*)srcImage);
+	cudaFree((void*)gainImage);
+	cudaFree((void*)dstImage);
+}
+
+template<> void
+launchCudaProcess<short>(int imgW, int imgH, int gridX, int gridY)
+{
+	short* srcImage, *gainImage, *dstImage;
+	int s = imgW*imgH;
+
+	cudaMalloc((short**)&srcImage, (s*sizeof(short)));
+	cudaMalloc((short**)&gainImage, (s*sizeof(short)));
+	cudaMalloc((short**)&dstImage, (s*sizeof(short)));
+
+	dim3 block(gridX, gridY, 1);
+	dim3 grid(imgW / block.x, imgH / block.y, 1);
+
+	auto start = std::chrono::system_clock::now();
+	launchCudaProcessHalf0(grid, block, 0, gainImage, srcImage, dstImage, imgW);
+	auto end  = std::chrono::system_clock::now();
+	auto dur = end - start;
+	int msec = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
+	std::cout << msec << " [us] consumed" << std::endl;
+
+	cudaFree((void*)srcImage);
+	cudaFree((void*)gainImage);
+	cudaFree((void*)dstImage);
 }
 
 
@@ -45,6 +70,9 @@ int main()
 {
 
 	launchCudaProcess<float>(1920, 1080, 16, 16);
+	launchCudaProcess<short>(1920, 1080, 16, 16);
+	launchCudaProcess<float>(640,  480,  16, 16);
+	launchCudaProcess<short>(640,  480,  16, 16);
 
 	return 0;
 }
